6ff19acd01f3510be836a4b43fda468d5b0e230b
muflax <muflax@gmail.com>
2010-02-14 19:47:27 +0100

    replaced wakeup script music with tv show :)

--- .wakeup.sh
+++ .wakeup.sh
@@ -1,5 +1,15 @@
 #!/bin/zsh
-#amixer -q set Software 85%
-ossmix -q vmix0-outvol 22
-mplayer -quiet -really-quiet -ao oss /home/amon/.wakeup &> /dev/null
-#boodler.py -o alsa computing.MultiComputing
+# variables
+export DISPLAY=:0.0 
+
+SHOW="ケロロ軍曹"
+PATTERN="*#{259..300}.*"
+
+SHOWPATH="/home/amon/テレビ/$SHOW"
+EP=$(eval "ls -1 $SHOWPATH/$PATTERN" | head -1)
+
+# play stuff
+ossmix -q vmix0-outvol 24
+xset dpms force on
+sleep 3
+mplayer -quiet -really-quiet -ao oss "$EP" &> /dev/null
