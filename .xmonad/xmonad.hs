fa45ee7bbb049688c6173aafc43498920946d2ac
muflax <mail@muflax.com>
2011-12-02 04:31:35 +0100

    update, remember song

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -154,9 +154,9 @@ keys' conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
         spawn "ssh amon@azathoth -- amixer set Master -q 5+")
     
     -- mpc
-    , ((modm              , xK_c     ), 
-        spawn "MPD_HOST=192.168.1.15 mpc --no-status toggle")
-    
+    , ((modm              , xK_c     ), spawn "MPD_HOST=192.168.1.15 mpc --no-status toggle")
+    , ((modm .|. shiftMask, xK_c     ), spawn "remember_song.sh")
+
     -- screenshots
     , ((modm .|. shiftMask, xK_o     ), 
         spawn "$HOME/in/scripts/selection")
