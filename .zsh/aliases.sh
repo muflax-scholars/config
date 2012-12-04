2751b6e644e1f4975a72733606ea10d79aca8acc
muflax <mail@muflax.com>
2012-12-04 14:32:05 +0100

    dipa improvs

--- .zsh/aliases.sh
+++ .zsh/aliases.sh
@@ -23,7 +23,7 @@ alias un='unison kira'
 alias ur='unison kira-repo'
 alias uep='sudo unison kira-portage -times -log=false -auto -ui=text'
 alias uo='unison kira-ongaku'
-alias udipa='sudo unison kira-dipa -times -log=false -auto -ui=text'
+alias udipa='sudo unison kira-dipa -times -log=false -auto -ui=text -prefer ssh://root@azathoth//var/cache'
 
 # gist
 alias gist="jist -t $(git config github.oauth-token)"
