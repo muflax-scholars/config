ba64121777af6e1a9b8c683092b9d5e88c43332a
muflax <mail@muflax.com>
2013-05-14 05:14:43 +0200

    full sync as 'fun'

--- .zsh/local_nyarlathotep.sh
+++ .zsh/local_nyarlathotep.sh
@@ -9,11 +9,22 @@ function up() {
     
     # eix
     eix-update
+ }
 
-    # clean-up
-    # echo -n "cleaning up..."
-    # azash "find ~/ -mount -name '*.unison.tmp*' -delete" &
-    # find ~/ -mount -name "*.unison.tmp*" -delete &
-    # wait
-    # echo "done."
+function fun() {
+  # full sync of laptop
+
+  # sync configs
+  un
+
+  # sync git-annex
+  git_annex_sync.sh
+  git_annex_get_auto.sh
+
+  # clean-up
+  echo -n "cleaning up..."
+  azash "find ~/ -mount -name '*.unison.tmp*' -delete" &
+  find ~/ -mount -name "*.unison.tmp*" -delete &
+  wait
+  echo "done."
 }
