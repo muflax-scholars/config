ba0fceaa41a611240b2caa8c2763499d8c05fc44
muflax <mail@muflax.com>
2013-05-16 01:41:01 +0200

    don't do clean-up after full sync

--- .zsh/local_typhus.sh
+++ .zsh/local_typhus.sh
@@ -22,9 +22,9 @@ function fun() {
   git_annex_get_auto.sh
 
   # clean-up
-  echo -n "cleaning up..."
-  azash "find ~/ -mount -name '*.unison.tmp*' -delete" &
-  find ~/ -mount -name "*.unison.tmp*" -delete &
-  wait
-  echo "done."
+  # echo -n "cleaning up..."
+  # azash "find ~/ -mount -name '*.unison.tmp*' -delete" &
+  # find ~/ -mount -name "*.unison.tmp*" -delete &
+  # wait
+  # echo "done."
 }
