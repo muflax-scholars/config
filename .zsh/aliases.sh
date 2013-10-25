a7564c5c846541ad0c8f6737649141efa9515bc2
muflax <mail@muflax.com>
2013-10-25 21:29:29 +0100

    firefox alias

--- .zsh/aliases.sh
+++ .zsh/aliases.sh
@@ -19,8 +19,9 @@ alias diff="colordiff"
 alias my="sudo chown amon:amon"
 alias x="chmod +x"
 alias po="popd"
-function ok() { okular $* >/dev/null 2>/dev/null &! }
+function ok() { okular      $* >/dev/null 2>/dev/null &! }
 function li() { libreoffice $* >/dev/null 2>/dev/null &! }
+function ff() { firefox     $* >/dev/null 2>/dev/null &! }
 
 # ls
 alias ls="ls --color=always --group-directories-first -v"
