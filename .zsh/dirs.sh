e645ba785a6e811a41f344ba7090cb48c86d1e45
muflax <mail@muflax.com>
2013-10-30 22:03:43 +0000

    actually include dirs.sh -_-

---
+++ .zsh/dirs.sh
@@ -0,0 +1,28 @@
+# named directories
+typeset -A NAMED_DIRS
+NAMED_DIRS=(
+  0g             ~/Dropbox/0german
+  gdict          ~/src/linguistics/german_dictionaries
+  gems           $(gem environment gemdir)/gems
+  german         ~/spoiler/languages/german
+  greengen       ~/src/instruction/german_greengen
+  lets           ~/Dropbox/LET\'S\ SING\ THE\ SHARING\ SONG\ YAY
+  maff           ~/Dropbox/0german/canoonet_grammar/
+  nfs            /mnt/nfs/
+  plocal         /usr/local/portage/local
+  src            ~/src
+)
+for key in ${(k)NAMED_DIRS}
+do
+  # only add paths that actually exist
+  if [[ -d ${NAMED_DIRS[$key]} ]]; then
+    hash -d $key=${NAMED_DIRS[$key]}
+  fi
+done
+
+function lsdirs () {
+  for key in ${(k)NAMED_DIRS}
+  do
+    printf "%s\t%s\n" $key  ${NAMED_DIRS[$key]}
+  done | sort
+}
