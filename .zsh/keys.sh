45aa02bcd4fa2c121dc3c224cb615683b4fb3681
muflax <mail@muflax.com>
2013-11-19 22:22:50 +0000

    fucked up zsh so I can fuck up emacs later so C-Backspace will eventually work LIKE IT FUCKING ALREADY SHOULD

--- .zsh/keys.sh
+++ .zsh/keys.sh
@@ -1,9 +1,9 @@
 # key bindings
 
-bindkey -e 
-bindkey '^[[3~' delete-char
-bindkey '[1~' beginning-of-line
-bindkey '^[[4~' end-of-line
-bindkey '^[[5~' beginning-of-history
-bindkey '^[[6~' end-of-history
-
+bindkey -e
+bindkey '^[[3~'  delete-char
+bindkey '[1~'  beginning-of-line
+bindkey '^[[4~'  end-of-line
+bindkey '^[[5~'  beginning-of-history
+bindkey '^[[6~'  end-of-history
+bindkey '^[[33~' backward-kill-word
