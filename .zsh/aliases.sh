930a3c145ad7aa6bf2c43669dd1f63660425e379
muflax <mail@muflax.com>
2013-11-25 17:42:26 +0000

    meld shortcut

--- .zsh/aliases.sh
+++ .zsh/aliases.sh
@@ -24,6 +24,7 @@ function li()  { libreoffice         $* >/dev/null 2>/dev/null &! }
 function ff()  { firefox             $* >/dev/null 2>/dev/null &! }
 function ffw() { firefox -new-window $* >/dev/null 2>/dev/null &! }
 function ge()  { geeqie              $* >/dev/null 2>/dev/null &! }
+function me()  { meld                $* >/dev/null 2>/dev/null &! }
 
 # ls
 alias ls="ls --color=always --group-directories-first -v"
