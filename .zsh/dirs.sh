7a83ea996fbc0289dfebcf84e9003119b0244a2e
muflax <mail@muflax.com>
2013-12-15 13:34:58 +0000

    more named dirs

--- .zsh/dirs.sh
+++ .zsh/dirs.sh
@@ -3,15 +3,24 @@ typeset -A NAMED_DIRS
 NAMED_DIRS=(
   0g             ~/Dropbox/0german
   1t             ~/Dropbox/1toki\ pona
-  gdict          ~/src/linguistics/german_dictionaries/lists
-  gems           $(gem environment gemdir)/gems
-  german         ~/spoiler/languages/german
-  muflax         ~/src/ruby/muflax
   lets           ~/Dropbox/LET\'S\ SING\ THE\ SHARING\ SONG\ YAY
-  maff           ~/Dropbox/0german/canoonet_grammar/
-  nfs            /mnt/nfs/
+  maff           ~/Dropbox/0german/canoonet_grammar
+
+  nfs            /mnt/nfs
+  gems           $(gem environment gemdir)/gems
   plocal         /usr/local/portage/local
+  portage        /usr/portage
+
   src            ~/src
+  gdict          ~/src/linguistics/german_dictionaries/lists
+  muflax         ~/src/ruby/muflax
+  scripts        ~/src/scripts
+
+  german         ~/spoiler/languages/german
+  functionese    ~/spoiler/languages/functionese
+
+  bgerman        ~/txt/languages/pie/germanic/german
+  blojban        ~/txt/languages/conlangs/loglan/lojban
 )
 for key in ${(k)NAMED_DIRS}
 do
@@ -24,6 +33,6 @@ done
 function lsdirs () {
   for key in ${(k)NAMED_DIRS}
   do
-    printf "%10s --> %s\n" $key  ${NAMED_DIRS[$key]}
+    printf "%15s --> %s\n" $key  ${NAMED_DIRS[$key]}
   done | sort -b
 }
