3f7c1c9b057a1b25a92dacd5ff37743e8f5424dc
muflax <mail@muflax.com>
2013-12-18 15:07:06 +0000

    unroll rbenv invocation (and sanity-wrap it)

--- .zsh/path.sh
+++ .zsh/path.sh
@@ -15,6 +15,26 @@ export R_LIBS="$HOME/local/R"
 
 # rbenv loads most gems
 export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
+
+# unroll rbenv code for speedup
+if [[ -s ~/.rbenv ]]; then
+  export RBENV_SHELL=zsh
+  source '/home/amon/.rbenv/completions/rbenv.zsh'
+  rbenv() {
+    local command
+    command="$1"
+    if [ "$#" -gt 0 ]; then
+      shift
+    fi
+
+    case "$command" in
+      rehash|shell)
+        eval "`rbenv "sh-$command" "$@"`";;
+      *)
+        command rbenv "$command" "$@";;
+    esac
+  }
+fi
+
 # but prefer bundler's version if it exists
 export PATH="$HOME/.bundle/bin:$PATH"
