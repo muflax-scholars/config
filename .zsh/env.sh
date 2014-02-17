8403f4a296e6b901d2dbc59828aafdee2ffc4956
muflax <mail@muflax.com>
2014-02-17 20:10:51 +0000

    clean up LS_COLORS

--- .zsh/env.sh
+++ .zsh/env.sh
@@ -35,5 +35,48 @@ esac
 # go
 export GOPATH=$HOME/local/go
 
-LS_COLORS='no=00:fi=00:di=00;36:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:su=37;41:sg=30;43:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.bz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.rar=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:';
-export LS_COLORS
+
+# fancy ls_colors
+#
+# meta:
+# 0   = Default Colour
+# 1   = Bold
+# 4   = Underlined
+# 5   = Flashing Text
+# 7   = Reverse Field
+#
+# foreground:
+# 30  = Black
+# 90  = Dark Grey
+# 31  = Red
+# 91  = Light Red
+# 32  = Green
+# 92  = Light Green
+# 33  = Orange
+# 93  = Yellow
+# 34  = Blue
+# 94  = Light Blue
+# 35  = Purple
+# 95  = Light Purple
+# 36  = Cyan
+# 96  = Turquoise
+# 37  = Grey
+#
+# background:
+# 40  = Black
+# 100 = Dark Grey
+# 41  = Red
+# 101 = Light Red
+# 42  = Green
+# 102 = Light Green
+# 43  = Orange
+# 103 = Yellow
+# 44  = Blue
+# 104 = Light Blue
+# 45  = Purple
+# 105 = Light Purple
+# 46  = Cyan
+# 106 = Turquoise
+# 47  = Grey
+
+export LS_COLORS="*.rb=00;32:$LS_COLORS"
