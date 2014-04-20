b3fe6455da64882af63f580b04593265f01633ec
muflax <mail@muflax.com>
2014-04-20 07:56:21 +0100

    update zsh dirs

--- .zsh/dirs.sh
+++ .zsh/dirs.sh
@@ -1,31 +1,41 @@
 # named directories
+
 typeset -A NAMED_DIRS
 NAMED_DIRS=(
-  0g             ~/Dropbox/0german
-  1t             ~/Dropbox/1toki\ pona
-  lets           ~/Dropbox/LET\'S\ SING\ THE\ SHARING\ SONG\ YAY
-  maff           ~/Dropbox/0german/canoonet_grammar
+  # dropbox
+  0g              ~/Dropbox/0german
+  lets            ~/Dropbox/LET\'S\ SING\ THE\ SHARING\ SONG\ YAY
+  maff            ~/Dropbox/0german/canoonet_grammar
 
-  layman         /var/lib/layman
-  nfs            /mnt/nfs
-  plocal         /usr/local/portage/local
-  portage        /usr/portage
+  # system stuff
+  layman          /var/lib/layman
+  nfs             /mnt/nfs
+  plocal          /usr/local/portage/local
+  portage         /usr/portage
 
-  src            ~/src
-  gdict          ~/src/linguistics/german/lists
-  muflax         ~/src/ruby/muflax
-  scripts        ~/src/scripts
-  ibus-steno     ~/src/typing/ibus/ibus-steno
-  mavothi        ~/src/instruction/mavothi
+  # random projects
+  src             ~/src
+  muflax          ~/src/ruby/muflax
+  scripts         ~/src/scripts
+  ibus-steno      ~/src/typing/ibus/ibus-steno
 
-  german         ~/spoiler/languages/german
-  functionese    ~/spoiler/languages/functionese
+  # german
+  dudenbib        ~/src/linguistics/german/dudenbib
+  duden           ~/src/linguistics/german/duden-analysis
+  gdict           ~/src/linguistics/german/lists
+  mavothi         ~/src/instruction/mavothi
+  script-rec      ~/src/instruction/script-recorder
 
-  bgerman        ~/txt/languages/pie/germanic/german
-  blatin         ~/txt/languages/pie/romance/latin
-  bjapanese      ~/txt/languages/isolates/japanese
-  brussian       ~/txt/languages/pie/slavic/russian
-  blojban        ~/txt/languages/conlangs/loglan/lojban
+  # notes
+  german          ~/spoiler/languages/german
+  gpreds          ~/spoiler/languages/german/predicates
+
+  # books
+  bgerman         ~/txt/languages/pie/germanic/german
+  blatin          ~/txt/languages/pie/romance/latin
+  bjapanese       ~/txt/languages/isolates/japanese
+  brussian        ~/txt/languages/pie/slavic/russian
+  blojban         ~/txt/languages/conlangs/loglan/lojban
 )
 for key in ${(k)NAMED_DIRS}
 do
